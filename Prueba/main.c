ad21e903b288eff39a0be61d719da0edae5d9e74
Lautaro <112659601+LautaroZambella@users.noreply.github.com>
2025-10-19 01:35:25 -0300

    filtros simultaneos
    anashe

--- Prueba/main.c
+++ Prueba/main.c
@@ -8,45 +8,110 @@ int main(int argc, char *argv[])
     printf("finish\n");
     return 0;
 }
-
-// Pixel calculoPromedio(Pixel** mat, int factor, int h_orig, int w_orig, int h_new, int w_new) {
-//     // factor: Cuántos píxeles de la imagen original corresponden a un píxel de la nueva imagen
-//     // h_new, w_new: Coordenadas del píxel en la NUEVA matriz
+/*
+FILE* p = fopen(arg, "rb");
+    if(p == NULL){
+        printf("error al abrir el archivo\n");
+        exit(1);
+    }
+    char newName[255] = "facistol_tonalidad_roja";
+    strcat(newName, arg);
+    FILE* fo = fopen(newName, "wb"); // IMPORTANT
+    if (fo == NULL){
+        printf("error al abrir el nuevo archivo\n");
+        exit(1);
+    }
+    BMPFileHeader* file = malloc(sizeof(BMPFileHeader));
+    BMPInfoHeader* info = malloc(sizeof(BMPInfoHeader));
+    facistolLeerHeader(p, file, info);
     
-//     // Calcula la esquina superior izquierda del bloque en la imagen original
-//     int start_h = h_new * factor;
-//     int start_w = w_new * factor;
-    
-//     // Determina el final del bloque a promediar, asegurando no exceder los límites
-//     // de la imagen original (h_orig, w_orig).
-//     int end_h = start_h + factor < h_orig ? start_h + factor : h_orig;
-//     int end_w = start_w + factor < w_orig ? start_w + factor : w_orig;
+    int32_t height = abs(info->height);
+    int is_top_down = (info->height < 0);
+    int padding = (4 - (info->width * 3) % 4) % 4;
+    int size = abs(info->height) * (info->width);
 
-//     long acuRed = 0;
-//     long acuGreen = 0;
-//     long acuBlue = 0;
-//     int count = 0; // Contador de píxeles promediados
+    Pixel** m = (Pixel**)facistolCrearMatriz(height, info->width, sizeof(Pixel));
+    fseek(p, file->offset_data, SEEK_SET);
+    readOrWriteMatrix(LEER, is_top_down, height, info->width, m, p, padding);
 
-//     for (int i = start_h; i < end_h; i++) {
-//         for (int j = start_w; j < end_w; j++) {
-//             acuRed += mat[i][j].red;
-//             acuGreen += mat[i][j].green;
-//             acuBlue += mat[i][j].blue;
-//             count++;
-//         }
-//     }
+    fwrite(file, sizeof(BMPFileHeader), 1, fo);
+    fwrite(info, sizeof(BMPInfoHeader), 1, fo);
+    fseek(fo, file->offset_data, SEEK_SET);
 
-//     Pixel devolver;
-//     if (count > 0) {
-//         // CORRECCIÓN: Dividir por el número TOTAL de píxeles promediados (count),
-//         // no por 3 (número de canales).
-//         devolver.red = (uint8_t)(acuRed / count);
-//         devolver.green = (uint8_t)(acuGreen / count);
-//         devolver.blue = (uint8_t)(acuBlue / count);
-//     } else {
-//         // En caso de error (no debería pasar con límites correctos)
-//         devolver.red = devolver.green = devolver.blue = 0;
-//     }
-    
-//     return devolver;
-// }
+
+    readOrWriteMatrix(ESCRIBIR, is_top_down, height, info->width, m, fo, padding);
+
+    facistolDestruirMatriz((void**)m, height);
+    fclose(p);
+    free(file);
+    free(info);
+    fclose(fo);*/
+
+/*void facistolAchicar(Pixel** mat, BMPFileHeader* file, BMPInfoHeader* info, char* arg, int porcentaje){
+if(porcentaje < 1 || porcentaje > 100){
+printf("Porcentaje no valido\n");
+porcentaje = 100;  // Valor por defecto
+}
+float factor = (100.0 / (float)porcentaje);  // Factor de escalado
+
+char newName[255] = "achicado_";
+strcat(newName, arg);
+FILE* fo = fopen(newName, "wb");  // Modo binario
+if (fo == NULL){
+    printf("error al abrir el nuevo archivo\n");
+    exit(1);
+}
+int32_t origenH = abs(info->height);
+int32_t origenW = info->width;
+
+int size = abs(info->height) * (info->width);
+int is_top_down = (info->height < 0);
+
+int32_t height = (int32_t)(origenH / factor);  // Altura nueva
+int32_t width = (int32_t)(origenW / factor);   // Ancho nuevo
+
+Pixel** newMat = (Pixel**)facistolCrearMatriz(height, width, sizeof(Pixel));
+
+for(int h = 0; h < height; h++){
+    for(int w = 0; w < width; w++){
+        newMat[h][w] = calculoPromedio(mat, factor, h, w, origenH, origenW, height, width);  // Pasa factor correcto
+    }
+}
+
+int padding = (4 - (width * 3) % 4) % 4;
+uint32_t imageSizeNew = (uint32_t)(height * (width * 3 + padding));
+uint32_t fileSizeNew = file->offset_data + imageSizeNew;
+
+file->file_size = fileSizeNew;
+info->image_size = imageSizeNew;
+
+if(is_top_down){
+    info->height = 0 - height;  // Mantener el signo
+}else{
+    info->height = height;
+}
+info->width = width;
+
+fwrite(file, sizeof(BMPFileHeader), 1, fo);
+fwrite(info, sizeof(BMPInfoHeader), 1, fo);
+fseek(fo, file->offset_data, SEEK_SET);
+
+if(is_top_down){
+    for(int k = 0; k < height; k++){
+        fwrite(newMat[k], sizeof(Pixel), info->width, fo);
+        for (int i = 0; i < padding; i++) {
+            fputc(0x00, fo);
+        }
+    }
+}else{
+    for(int k = height -1; k >= 0; k--){
+        fwrite(newMat[k], sizeof(Pixel), info->width, fo);
+        for (int i = 0; i < padding; i++) {
+            fputc(0x00, fo);
+        }
+    }
+}
+
+facistolDestruirMatriz((void**)mat, origenH);
+facistolDestruirMatriz((void**)newMat, height);
+fclose(fo); */
